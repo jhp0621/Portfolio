bc155db99b96c61023a2a60f9ab0182b12329178
Ji Hye Park <jhp0621@gmail.com>
2020-06-28 23:07:17 -0500

    projects page halfway done

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import kite from '../assets/img/kite2.jpg';
+import kite from '../assets/img/kite.png';
 import magik from '../assets/img/magik.png';
 
 const IndexPage = () => {
@@ -15,13 +15,41 @@ const IndexPage = () => {
           <div className="inner">
             <div id="section1">
               <h3>Kite Games</h3>
-              <h6>Full Stack Developer | April 2020</h6>
+              <h6>Full Stack Developer | April '20</h6>
               <span className="image left">
                 <img src={kite} alt="" />
+                 <br />
+
+                <ul className="icons">
+                  <li>
+                    <a target="_blank"
+                      href="https://github.com/AJL-Cap/Kite/"
+                      style={{ borderBottom: 'none' }}
+                    >
+                      <i className="fab fa-github fa-2x" />
+                    </a>
+                  </li>
+                  <li>
+                    <a target="_blank"
+                      href="http://www.kite.games/"
+                      style={{ borderBottom: 'none' }}
+                    >
+                      <i class="fas fa-link fa-2x" />
+                    </a>
+                  </li>
+                  <li>
+                    <a target="_blank"
+                      href="https://youtu.be/ECHcCAnJn0E"
+                      style={{ borderBottom: 'none' }}
+                    >
+                      <i className="fab fa-youtube fa-2x" />
+                    </a>
+                  </li>
+                </ul>
               </span>
               <p>
                 Kite is a multiplayer realtime games website built within 2.5
-                weeks by a team of 3 developers, using Firebase, React, Express and Node. As social distancing quickly
+                weeks by a team of three developers, using Firebase, React, Express and Node. As social distancing quickly
                 became the norm, we were inspired to create a virtual space
                 where friends can enjoy each other's company without
                 having to be in the same room.
@@ -35,51 +63,47 @@ const IndexPage = () => {
               </p>
               <p>
                 My contributions for the project include: <br/>• building a variety of
-                components and utility functions for games <br/>• setting up user
-                authentication & profile<br/>• using Cloudinary and Signature Canvas
-                for image processing and <br/>• implementing sound effects for games,
+                components and utility functions for games, <br/>• setting up user
+                authentication, OAuth & profile, <br/>• using Cloudinary and Signature Canvas
+                for image processing, and <br/>• implementing sound effects for games,
                 chats, and notifications.
-              </p>{' '}
-              <p>
+              </p>
+              <hr />
+            </div>
+            <div id="section2">
+              <h3>M.A.G.I.K </h3>
+              <h6>Sole Developer | April '20</h6>
+              <span className="image left">
+                <img src={magik} alt="" />
+                <br />
+
                 <ul className="icons">
                   <li>
-                    <a
-                      href="https://github.com/AJL-Cap/Kite/"
+                    <a target="_blank"
+                      href="https://github.com/jhp0621/M.A.G.I.K"
                       style={{ borderBottom: 'none' }}
                     >
                       <i className="fab fa-github fa-2x" />
                     </a>
                   </li>
                   <li>
-                    <a
-                      href="http://www.kite.games/"
-                      style={{ borderBottom: 'none' }}
-                    >
-                      <i class="fas fa-link fa-2x" />
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://youtu.be/ECHcCAnJn0E"
+                    <a target="_blank"
+                      href=""
                       style={{ borderBottom: 'none' }}
                     >
                       <i className="fab fa-youtube fa-2x" />
                     </a>
                   </li>
                 </ul>
-              </p>
-              <hr />
-            </div>
-            <div id="section2">
-              <h3>M.A.G.I.K </h3>
-              <h6>Sole Developer | April 2020</h6>
-              <span className="image left">
-                <img src={magik} alt="" />
               </span>
-              <p>blah blah</p>
+              <p>M.A.G.I.K is an iOS, Android mobile app I created during a 4-day hackathon using React Native, Expo.io, and Google Cloud Speech-to-Text & Text-to-Speech APIs. The name stands for <strong>M</strong>indfulness, <strong>A</strong>ffirmations app for users to practice <strong>G</strong>ratitude, feel <strong>I</strong>nspired, and be <strong>K</strong>ind to themselves. </p>
+              <p>I am big fan of meditation and I use <a target="_blank" href="https://insighttimer.com/">Insight Timer</a> daily. If there is one feature I could add to the app, it would be allowing users to customize affirmations they can meditate to. This is why I built M.A.G.I.K! On M.A.G.I.K, users will record their own affirmations, choose the voice preference for gender, pitch and speed, and finally, meditate to their personal mantra spoken by the AI voice of their choice (or their own voice), on repeat, combined with the sound of soothing music. </p>
+              <p>Try it out for yourself! Be as specific as you can and visualize yourself achieving your goals. These are my affirmations of the day: "Today I am going to stay motivated, focused and productive. I am going to get at least 15k steps in and make substantial progress on this portfolio website."</p>
             </div>
+            <hr />
             <div id="section3">
               <h3>Graced Pickles</h3>
+              <h6>Full Stack Developer | March '20</h6>
               <p>blah blah</p>
             </div>
             <div id="section4">
